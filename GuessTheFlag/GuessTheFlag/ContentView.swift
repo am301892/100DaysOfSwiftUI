7b2ad43440f0c59e582b2a4406f4c258e7149401
am301892 <am301892@student.polsl.pl>
2024-09-10 23:59:35 +0200

    day21

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,43 +8,78 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingAlert = false
+//    @State private var showingAlert = false
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+
+    var correctAnswer = Int.random(in: 0...2)
     
     var body: some View {
-        VStack {
-            Button("Show Alert") {
-                        showingAlert = true
+       
+        ZStack{
+            Color.blue
+                .ignoresSafeArea()
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundStyle(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundStyle(.white)
+                }
+                
+                ForEach(0..<3) { number in
+                    Button {
+                        flagTapped(number)
+                    } label: {
+                        Image(countries[number])
                     }
-                    .alert("Important message", isPresented: $showingAlert) {
-                        Button("OK") { }
-                    }
-            
-            Button("Button 1") { }
-                .buttonStyle(.bordered)
-                .tint(.mint)
-            Button("Button 2", role: .destructive) { }
-                .buttonStyle(.bordered)
-            Button("Button 3") { }
-                .buttonStyle(.borderedProminent)
-            Button("Button 4", role: .destructive) { }
-                .buttonStyle(.borderedProminent)
-            Button {
-                print("Button was tapped")
-            } label: {
-                Text("Tap me!")
-                    .padding()
-                    .foregroundStyle(.white)
-                    .background(.indigo)
+                }
             }
-            Button {
-                print("Edit button was tapped")
-            } label: {
-                Image(systemName: "pencil")
-            }
-            Button("Edit", systemImage: "pencil") {
-                print("Edit button was tapped")
+            .alert(scoreTitle, isPresented: $showingScore) {
+                Button("Continue"){
+                    askQuestion()
+                }
+            } message: {
+                Text("Your score is ???")
             }
         }
+//        VStack {
+//            Button("Show Alert") {
+//                        showingAlert = true
+//                    }
+//                    .alert("Important message", isPresented: $showingAlert) {
+//                        Button("OK") { }
+//                    }
+//            
+//            Button("Button 1") { }
+//                .buttonStyle(.bordered)
+//                .tint(.mint)
+//            Button("Button 2", role: .destructive) { }
+//                .buttonStyle(.bordered)
+//            Button("Button 3") { }
+//                .buttonStyle(.borderedProminent)
+//            Button("Button 4", role: .destructive) { }
+//                .buttonStyle(.borderedProminent)
+//            Button {
+//                print("Button was tapped")
+//            } label: {
+//                Text("Tap me!")
+//                    .padding()
+//                    .foregroundStyle(.white)
+//                    .background(.indigo)
+//            }
+//            Button {
+//                print("Edit button was tapped")
+//            } label: {
+//                Image(systemName: "pencil")
+//            }
+//            Button("Edit", systemImage: "pencil") {
+//                print("Edit button was tapped")
+//            }
+//        }
+        
+        
 //        ZStack {
 //            VStack(spacing: 0) {
 //                Color.red
@@ -83,6 +118,19 @@ struct ContentView: View {
         //  .background(.red)
         // .padding()
         //   }
+    }
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+
+        showingScore = true
+    }
+    mutating func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
