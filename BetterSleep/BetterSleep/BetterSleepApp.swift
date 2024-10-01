fa91de0d8c7b667f00bd3dae08ecaa07b1076d63
am301892 <am301892@student.polsl.pl>
2024-10-01 23:03:29 +0200

    day 26

---
+++ BetterSleep/BetterSleep/BetterSleepApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterSleepApp.swift
+//  BetterSleep
+//
+//  Created by Aleksandra Maksimowska on 01/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterSleepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterSleep/BetterSleep/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BetterSleep
+//
+//  Created by Aleksandra Maksimowska 
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
