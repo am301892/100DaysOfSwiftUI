f2fed8ab912677ebeeefc5f1f1bc74a0342e9eab
am301892 <am301892@student.polsl.pl>
2024-11-11 23:09:20 +0100

    day30

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -2,14 +2,38 @@
 //  ContentView.swift
 //  WordScramble
 //
-//  Created by Aleksandra Maksimowska on 16/10/2024.
+//  Created by Aleksandra Maksimowska 
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    
     var body: some View {
-        VStack {
+        NavigationStack {
+            List{
+                Text(rootWord)
+                Section{
+                    TextField("Enter your word", text: $newWord)
+                        .textInputAutocapitalization(.never)
+                }
+                
+                Section{
+                    ForEach(usedWords, id: \.self){
+                        word in HStack{
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
+                }
+            }
             //bundle.main.url() -> optional
 //            if let fileURL = Bundle.main.url(forResource: "some-file", withExtension: "txt")
             //String(contentsOf: ) -> it will send back a string containing the contents of that file if it can be loaded (else error)
@@ -32,31 +56,110 @@ struct ContentView: View {
             
         //it returns NSNotFound as it has no optionals (Objective-c)
             //let allGood = misspelledRange.location ==NSNotFound
-            List {
-                Section("Section 1") {
-                    Text("Static row 1")
-                    Text("Static row 2")
-                }
-
-                Section("Section 2") {
-                    ForEach(0..<5) {
-                        Text("Dynamic row \($0)")
-                    }
-                }
-
-                Section("Section 3") {
-                    Text("Static row 3")
-                    Text("Static row 4")
-                }
-            }
-            .listStyle(.grouped)
+//            List {
+//                Section("Section 1") {
+//                    Text("Static row 1")
+//                    Text("Static row 2")
+//                }
+//
+//                Section("Section 2") {
+//                    ForEach(0..<5) {
+//                        Text("Dynamic row \($0)")
+//                    }
+//                }
+//
+//                Section("Section 3") {
+//                    Text("Static row 3")
+//                    Text("Static row 4")
+//                }
+//            }
+//            .listStyle(.grouped)
+//            
+//            List(0..<5){
+//                Text("Dynamic text \($0)")
+//            
+//            }
             
-            List(0..<5){
-                Text("Dynamic text \($0)")
-            
+        }
+        .navigationTitle(rootWord)
+        //We want to call addNewWord() when the user presses return on the keyboard
+        //onSubmit() needs to be given a function that accepts no parameters and returns nothing
+        .onSubmit(addNewWord)
+        .onAppear(perform: startGame)
+        .alert(errorTitle, isPresented: $showingError){
+            Button("OK"){}
+        }message: {
+            Text(errorMessage)
+        }
+    }
+    func addNewWord(){
+        //Lowercase newWord and remove any whitespace
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        //Check that it has at least 1 character otherwise exit
+        guard answer.count > 0 else {return}
+        //Insert that word at position 0 in the usedWords array and Set newWord back to be an empty string
+        
+        guard isOriginal(word: answer) else{
+            wordError(title: "Word used already", message: "Be more original")
+        }
+        
+        guard isPossible(word: answer) else{
+            wordError(title: "Word not possible", message: "You cant spell that word from \(rootWord)")
+        }
+        
+        guard isReal(word: answer) else{
+            wordError(title: "Word not recognized", message: "you cant just make them up, you know!")
+        }
+        withAnimation{
+            usedWords.insert(answer, at: 0)
+        }
+        newWord = ""
+    }
+    
+    func startGame(){
+        //Find start.txt in our bundle
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt"){
+            //Load it into a string
+            if let startWords = try? String(contentsOf: startWordsURL){
+                // Split that string into array of strings, with each element being one word
+                let allWords = startWords.components(separatedBy: "\n")
+                // Pick one random word from there to be assigned to rootWord, or use a sensible default if the array is empty.
+                rootWord = allWords.randomElement() ?? "silkworm"
+                return
             }
         }
-        .padding()
+        fatalError("Could not load start.txt from bundle")
+    }
+    
+    
+    func isOriginal(word: String) -> Bool{
+        !usedWords.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool{
+        var tempWord = rootWord
+        
+        for letter in word{
+            if let pos = tempWord.firstIndex(of: letter){
+                tempWord.remove(at: pos)
+            }else{
+                return false
+            }
+        }
+        return true
+    }
+    
+    func isReal(word: String) -> Bool{
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func wordError(title: String, message: String){
+        errorTitle = title
+        errorMessage = messageshowingError = true
     }
 }
 
